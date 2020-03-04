a3e03db3c4040dde7cf168420c34adcd4794a817
Dymu <patryk-dym@o2.pl>
2020-03-04 21:08:48 +0100

    Dodaj pliki projektów.

---
+++ SI_Lab_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace SI_Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string f = "berlin11_modified.tsp";
+
+            var lines = File.ReadAllLines(f);
+
+            foreach(var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+    }
+}
